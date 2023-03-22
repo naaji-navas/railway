1f19474e013fd92765b06e5161886ee1af928fa2
naaji-navas <najidnavas2000@gmail.com>
2023-03-22 19:45:11 +0530

    added the razorpay and the pdf button. but not the functionality

--- src/common/UI/UserDetails/index.js
+++ src/common/UI/UserDetails/index.js
@@ -1,8 +1,10 @@
+import { Button } from "@mui/material";
 import { useRouter } from "next/router";
 import { useContext, useEffect, useState } from "react";
 import { Message_data } from "../../../../context/context";
 
 const UserDetails = () => {
+  const [paid, setPaid] = useState(true);
   const [user, setUser] = useState({});
   const [message, setMessage] = useState(localStorage.getItem("token"));
   const apiUrl = "https://ima-msn.up.railway.app/current_user/";
@@ -26,7 +28,7 @@ const UserDetails = () => {
   }, [apiUrl, message]);
 
   return (
-    <div className="bg-white rounded-lg shadow-lg p-6 max-w-[50%] mx-auto text-ellipsis overflow-hidden">
+    <div className="bg-white rounded-lg shadow-lg p-6 max-w-[80%] align-middle justify-center mt-[20%] mx-auto text-ellipsis overflow-hidden">
       <h1 className="text-ellipsis overflow-hidden text-2xl font-bold mb-4">
         User Details
       </h1>
@@ -45,6 +47,15 @@ const UserDetails = () => {
         <div>{user.alt_email_id}</div>
         <div className="font-semibold">Preferred Location:</div>
         <div>{user.pref_loc}</div>
+        {paid ? (
+          <Button id="rzp-button1" variant="contained">
+            Pay with Razorpay
+          </Button>
+        ) : (
+          <Button id="rzp-button1" variant="contained">
+            Download PDF
+          </Button>
+        )}
       </div>
     </div>
   );
