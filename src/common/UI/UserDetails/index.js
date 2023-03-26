de48e8163e41207831557b8f8a92ff46473a0f65
naaji-navas <najidnavas2000@gmail.com>
2023-03-26 18:12:45 +0530

    cleared the responsive issue

--- src/common/UI/UserDetails/index.js
+++ src/common/UI/UserDetails/index.js
@@ -205,15 +205,15 @@ const UserDetails = () => {
   return (
     <div className="my-20-5  overflow-y-scroll">
 
-      <div className="flex md:flex-row  flex-col justify-center items-center ">
+      <div className="flex md:flex-row flex-col justify-center items-center">
         <h1 className="text-3xl font-bold text-indigo-700 mb-8">
           IMA MSN
         </h1>
       </div>
-      <div className="flex md:flex-row  flex-col justify-center gap-3 items-center h-screen">
+      <div className="flex md:flex-row  flex-col justify-center gap-3 items-center ">
         {/* Program Details */}
         {paid ? (
-          <div className="bg-white rounded-lg hidden md:block shadow-lg p-6 max-w-3xl mx-auto ">
+          <div className="bg-white rounded-lg  shadow-lg p-6 max-w-3xl mx-auto ">
             <h1 className="text-2xl font-bold mb-4 text-center text-indigo-700">
               Program Details
             </h1>
@@ -263,42 +263,23 @@ const UserDetails = () => {
               </Button>
             )}
             <div className="text-center sm:text-right">
-              <Button onClick={() => signOut()} className="bg-red-600 text-white rounded-md px-4 py-2 hover:bg-red-500" variant="contained">
+              <Button onClick={() => signOut()} className="bg-red-600 text-white rounded-md px-4 py-2 hover:bg-red-500 ">
                 Sign Out
               </Button>
             </div>
           </div>
         </div>
-        {paid ? (
-          <div className="bg-white rounded-lg shadow-lg p-6 md:hidden block max-w-3xl mx-auto ">
-            <h1 className="text-2xl font-bold mb-4 text-center text-indigo-700">
-              Program Details
-            </h1>
-            <div className="flex flex-col gap-4 text-lg">
-              <div className="bg-indigo-100 p-4 rounded-md">8 to 8.30 inauguration</div>
-              <div className="bg-indigo-200 p-4 rounded-md">8.30 to 9.30 talk by toppers</div>
-              <div className="bg-indigo-300 p-4 rounded-md">9.30 to 10 talk by psychiatrist</div>
-              <div className="bg-indigo-400 p-4 rounded-md">10.30 to 1.30 exam</div>
-            </div>
-            <div className="justify-center mt-6">
-              <a href="https://chat.whatsapp.com/HacGrs1p4h56I84MMt3bkS" target="_blank" rel="noopener noreferrer">
-                <button className="bg-indigo-700 hover:bg-indigo-800 text-white font-bold py-2 px-4 rounded-full">
-                  Join our Whatsapp Group
-                </button>
-              </a>
-            </div>
-          </div>
-        ) : null}
+
         {paid ? (<div className="bg-white rounded-lg shadow-lg p-2 min-w-[40%] align-middle justify-center  mx-auto">
             <h1 className="text-2xl text-indigo-700 font-bold mb-4 text-center">
               Location Details
             </h1>
             <div className="grid grid-cols-1 md:grid-cols-1 gap-4">
               {user.pref_loc == 'Kochi' ? (
-                <div className="flex justify-center items-center h-[400px]">
+                <div className="flex justify-center p-5 items-center h-[400px]">
                   <iframe className="m-4" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d62865.98242584101!2d76.28940321683658!3d10.006623109668615!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b080c058ff7a4cd%3A0xa9c697fee1e03c65!2sGovernment%20Medical%20College%20Ernakulam!5e0!3m2!1sen!2sin!4v1679807141846!5m2!1sen!2sin" width="100%" height="100%" style={{border: 0}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
                 </div>
-              ) : (<div className="flex justify-center items-center h-[400px]">
+              ) : (<div className="flex justify-center items-center h-[400px] p-5">
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d62865.98242584101!2d76.28940321683658!3d10.006623109668615!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b080c058ff7a4cd%3A0xa9c697fee1e03c65!2sGovernment%20Medical%20College%20Ernakulam!5e0!3m2!1sen!2sin!4v1679807141846!5m2!1sen!2sin" width="600" height="450" style={{border: 0}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
               </div>)}
             </div>
