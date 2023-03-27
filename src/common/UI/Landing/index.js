d349a15b0a43d2acff361e4e4c0f1d44bb20e9fe
naaji-navas <najidnavas2000@gmail.com>
2023-03-27 06:20:16 +0530

    may work

--- src/common/UI/Landing/index.js
+++ src/common/UI/Landing/index.js
@@ -36,11 +36,37 @@ const Cognosco =() =>{
       </div>
       <div className="map py-28 px-10 bg-purple-100">
         <h1 className="text-4xl font-bold mb-8">Centres</h1>
-        <div className="flex justify-between">
-          <div className="w-1/2 pr-12">
-            <p className="text-lg mb-4">Gmc Trivandrum <b>(700)</b> slots</p>
-          </div>
-        </div>
+      <div className="flex flex-col md:flex-row">
+  <div className="p-4">
+    <div className="mb-4">
+      <p>Gmc Trivandrum <b>(700)</b> slots</p>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3945.7498762846944!2d76.92607644996733!3d8.523647493842343!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b05bbfd65530ccd%3A0xa0ceab851a9f2e90!2sGovernment%20Medical%20College%20Thiruvananthapuram!5e0!3m2!1sen!2sin!4v1679840329617!5m2!1sen!2sin"
+        width="400"
+        height="400"
+        style={{ border: 0 }}
+        allowFullScreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </div>
+  </div>
+  <div className="  p-4">
+    <div className="mb-4">
+      <p>Gmc Ernakulam <b>(300)</b> slots</p>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d62865.98242584101!2d76.28940321683658!3d10.006623109668624!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b080c058ff7a4cd%3A0xa9c697fee1e03c65!2sGovernment%20Medical%20College%20Ernakulam!5e0!3m2!1sen!2sin!4v1679840280418!5m2!1sen!2sin"
+        width="400"
+        height="400"
+        style={{ border: 0 }}
+        allowFullScreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </div>
+  </div>
+</div>
+
       </div>
       <div className="chart py-28 px-10">
         <h1 className="text-4xl font-bold mb-8">Statistics</h1>
