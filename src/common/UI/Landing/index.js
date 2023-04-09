74fe06400d8ba7eb279970736be7696e9af1a4bc
naaji-navas <najidnavas2000@gmail.com>
2023-04-10 02:00:29 +0530

    added the location kozhikode

--- src/common/UI/Landing/index.js
+++ src/common/UI/Landing/index.js
@@ -240,7 +240,7 @@ const Landing = () => {
         </div>
       </div>
       <div className="fixed bottom-0 w-full bg-blue-500 py-4 text-center rounded-t-2xl">
-        <p className="text-white"><span className="font-medium">Event date has been updated</span> , Ernakulam 14 April, Trivandrum and kozhikode april 16 </p>
+        <p className="text-white"><span className="font-medium">Event date has been updated</span> , Ernakulam 14 April. Trivandrum and kozhikode April 16 </p>
       </div>
       <section className="bg-white">
         <div className="max-w-screen-xl px-4 py-8 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8">
